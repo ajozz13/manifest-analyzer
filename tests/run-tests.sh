2ccda06221057dd678cbef230dca2438450ffde7
Alberto Ochoa <ajozz13@gmail.com>
2014-07-22 16:02:42 -0400

    add support for ruby and jruby

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,13 +2,23 @@
 
 ##run tests ruby -I. name_of_test_script
 ## Pass as arguments String with a test name and the name of the script file to run 
+
+PROG=ruby
+
 ruby_tester(){
         echo -----------------------------------------------------
         echo Run Tests for "$1"
-        ruby -I. $2
+        $PROG -I. $2
         wait
         read -p "The set has been completed.  Press Enter to continue." 
 }
+
+##Start Script
+if [ "$1" == "-j" ]; then
+	PROG=jruby
+fi
+
+$PROG -v
 
 #ruby_tester "Email Line Tester" email-line-tester.rb
 #ruby_tester "Manifest Line Tester" manifest-line-tester.rb

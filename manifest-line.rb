b341ad9a6b604a25e6392e71220c1b994d2ae29b
Alberto Ochoa <ajozz13@gmail.com>
2014-07-22 16:03:48 -0400

    setup manifest line tester for simple run and report of error messages

--- manifest-line.rb
+++ manifest-line.rb
@@ -3,18 +3,30 @@
 	http://www.pactrak.com/manifest-to-pactrak.html.  We will test a sub-standard that 
 	will support a HTTP rest driven approach, to submit and create the IBC standard format.
 =end
-
-#	Library requirements
+#	requirements
 require 'date'
+require 'csv'
 
 #	Global variables
-
 $manifest_line_headers = [ "record_type", "man_code", "man_date", "man_origin", "man_dest", "flight_info", "man_info" ]
 $airline_regex = %r{\A[A-Z]{2}\d{3,4}\z}
 $ibc_date_format = "%Y%m%d"
 $debug = false
 
 #	Functions
+def print_array arr
+        arr.each_with_index do |issue, index|
+                puts "#{ index+1 }: #{ issue }"
+        end
+end
+
+##      Print the fields with the entries that where read into the input array
+def print_manifest_line input_array
+        $manifest_line_headers.each_with_index do |header, index|
+                puts "#{ header } = #{ input_array[0][ index ] }"
+        end
+        puts
+end
 
 ##      Test the max length of a String and fail if it's empty
 def test_max_length input_str, name, max_size
@@ -35,7 +47,7 @@ end
 ##      Perform MAWB Tests
 def test_mawb input_str
 	if ( input_str.length != 11 )  
-		puts "MAWB must have 11 digits (#{ input_str.length })" 
+		puts "MAWB must have 11 digits (#{ input_str.length })" if $debug
 		return false
 	end
 	
@@ -44,7 +56,6 @@ def test_mawb input_str
 	#@noncd = Integer( input_str[3..input_str.length - 2] )  
 	@noncd = input_str[3..input_str.length-2].to_i 
 	@compare = ( @noncd - (@noncd / 7 * 7) ) == @cd
-	puts "MAWB Check digit is incorrect: #{ @cd }" unless @compare
 	return @compare
 end
 
@@ -53,5 +64,82 @@ def valid_airline_code? input_code
         return ( input_code =~ $airline_regex ) == 0
 end
 
+=begin
+        Test manifest line
+        Unit Test with main program below
+        See documentation page to see all the rules
+=end
+def test_manifest_line input_line
+        errors_found = []
+        begin
+                csv_array = CSV.parse( input_line )
+
+                if csv_array[0].size == $manifest_line_headers.size
+
+                unless csv_array[0][0].eql? "1"
+                        errors_found.push "Record type definition should start with 1"
+                end
+
+                ##Simple tests for length
+                unless test_max_length csv_array[0][1], "manifest code", 15  #maxlength for man_code is 15 
+                        errors_found.push "Manifest code: #{csv_array[0][1]} exceeds the maximum limit"
+                end
+
+                #test date given of manifest
+                unless test_date csv_array[0][2]
+                        errors_found.push "Date given: #{csv_array[0][2]} may be invalid or not in format expected: YYYYmmDD"
+                end
+
+                unless test_max_length csv_array[0][3], "origin code", 3  #maxlength for origin_code is 3 
+                        errors_found.push "Origin code: #{csv_array[0][3]} exceeds the maximum limit"
+                end
+
+                unless test_max_length csv_array[0][4], "destination code", 3  #maxlength for destination_code is 3 
+                        errors_found.push "Destination code: #{csv_array[0][4]} exceeds the maximum limit"
+                end
+
+                #test format of airline input
+                unless valid_airline_code? csv_array[0][5]
+                        errors_found.push "Airline info given: #{csv_array[0][5]} does not match expected format, IE: AA####"
+                end
+
+                unless test_mawb csv_array[0][6]
+                        errors_found.push "MAWB test failed. Please confirm the number used is valid."
+                end
+    
+        else
+                errors_found.push "Manifest line does not contain the required fields."
+        end
+        raise "Errors found" unless errors_found.empty?
+        @completed = true
+  rescue Exception => e
+        puts "Message: #{ e.message }"
+        print_array errors_found
+        print_manifest_line csv_array 
+        puts "Backtrace: #{ e.backtrace }" if $debug
+        @completed = false 
+  end
+
+end
+
+##      Run Test for test_manifest_line function
+=begin
+
+#       Requirements  csv library
+
+puts "Begin Test Program"
+
+input_lines = []
+input_lines.push "1,abc12345,20140701,YYZ,MIA,AA2012,18062148741"
+input_lines.push "1,abc12345,,YYZX,MIA,AASS2012,AB221111"
 
 
+input_lines.each_with_index do |l, index|
+        ans = test_manifest_line( l )
+        puts "Line ##{ index+1 } completed with answer = #{ ans }"
+        puts "-------------------------------------"
+end
+
+
+puts "End Test Program"  
+=end

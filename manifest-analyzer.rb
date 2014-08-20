b3e9cc15c0b0449b04797240d3de72e67136bc6f
Alberto Ochoa <ajozz13@gmail.com>
2014-08-20 14:50:35 -0400

    add reporting options and new manifest specific tests

--- manifest-analyzer.rb
+++ manifest-analyzer.rb
@@ -29,23 +29,35 @@ begin
         stop_processing = ARGV.include? "-s" #On first error line error
         f = File.open( ARGV[ f_index ], "r" )
         puts "Start Analysis for: #{ f.inspect }"
+        ##Counters and flags
+        man_line_detected = false
+        man_line_counter = 0
+        ship_line_counter = 0
         f.each_line do |line|
                 puts "Test #{ line }" if $debug
                 case line[ 0 ]
 
                         when "1"
                                 run_test{ test_manifest_line line }
+                                man_line_detected = true
+                                man_line_counter += 1
                         when "6","7"                
                                 run_test{ test_email_line line }
                         when "8","9"
                                 run_test{ test_shipment_line line }
+                                man_line_detected = false if man_line_detected
+                                ship_line_counter += 1
                         else
                                 raise "There is an issue processing this line\n[#{ line }]"
                 end
                 puts "Stop? #{ stop_processing } Pause Testing? #{ $errors_count > 0 }" if $debug
                 raise "Stop Processing. Total errors #{ $errors_count }" if stop_processing  && $errors_count > 0
+                
         end
-        raise "Errors reported. Total lines with errors #{ $errors_count }" if $errors_count > 0
+        puts "Total manifest reported: #{ man_line_counter }. Total shipments reported: #{ ship_line_counter }."
+        raise "A new manifest line was detected but no subsequent shipment lines." if man_line_detected
+        raise "We did not find any manifest lines to report." if man_line_counter < 1
+        raise "Errors reported. Total lines with errors #{ $errors_count }" if $errors_count > 0   
 rescue Exception => e
         puts "Exception: #{e}"
         puts "#{ e.backtrace }"

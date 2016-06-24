f25fa8899a55862196ea5c53bad2b059548c8f51
aochoa <aochoa@ajozz-mob2>
2016-06-24 17:28:52 -0400

    update email check test

--- email-line.rb
+++ email-line.rb
@@ -15,7 +15,7 @@ $debug = false
 #       http://www.sw-engineering-candies.com/blog-1/howtofindvalidemailaddresswitharegularexpressionregexinjava
 #$email_regex = %r{(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])}
 
-$email_regex = %r{\A[0-9a-z][0-9a-z.]+\w+@[0-9a-z][0-9a-z.]+\w+$}xi
+$email_regex = %r{\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+}xi
 
 =begin  Notes on the regex above
         For some reason the regex above crashes when tested with Jruby
